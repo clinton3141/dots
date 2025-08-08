3cf8e77b03dacd511e838fff5fac7bfa953ba036
Clinton Montague <clinton3141@gmail.com>
2025-08-08 21:17:59 +0100

    chore(cd): RTFM for zoxide

--- config/zsh/cd.zsh
+++ config/zsh/cd.zsh
@@ -1,15 +1,3 @@
 if command -v zoxide >/dev/null 2>&1; then
-    eval "$(zoxide init zsh)"
-
-    cd() {
-        if [ $# -eq 0 ]; then
-            builtin cd
-        elif [ "$1" = "-" ]; then
-            builtin cd -
-        elif [ -d "$1" ]; then
-            builtin cd "$@"
-        else
-            z "$@" 2>/dev/null || builtin cd "$@"
-        fi
-    }
+    eval "$(zoxide init zsh --cmd cd)"
 fi
