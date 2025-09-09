bd7418005939cbc31c1158370493260f7d750945
Clinton Montague <clinton3141@gmail.com>
2025-09-09 16:29:19 +0100

    chore(fzf): improve defaults

--- config/zsh/fzf.zsh
+++ config/zsh/fzf.zsh
@@ -1,4 +1,8 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if command -v fzf > /dev/null; then
+    source <(fzf --zsh)
+else
+    return
+fi
 
 export FZF_DEFAULT_OPTS="
     --height 40%
@@ -11,9 +15,12 @@ export FZF_DEFAULT_OPTS="
 
 export FZF_COMPLETION_TRIGGER='**'
 
-# Use fd (if available) for fzf file search
-if command -v fd > /dev/null; then
-    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+# Use rg > fd for file search (rg is faster for large repos)
+if command -v rg > /dev/null; then
+    export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!node_modules/*" --glob "!.cache/*"'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+elif command -v fd > /dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude node_modules --exclude .cache'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
