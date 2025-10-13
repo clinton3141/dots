ddb8dc7f22669a568bb8ae0a5f29241ae1bbd287
Clinton Montague <clinton3141@gmail.com>
2025-10-13 22:00:13 +0100

    fix: prevent vi mode overwriting history search bindings

--- config/zsh/history-substring-search.zsh
+++ config/zsh/history-substring-search.zsh
@@ -1,11 +1,13 @@
-zinit ice ver"$DOTS_ZSH_HISTORY_SUBSTRING_SEARCH_HASH"
+zinit ice wait"0" lucid atload"_history_substring_search_config" ver"$DOTS_ZSH_HISTORY_SUBSTRING_SEARCH_HASH"
 zinit load zsh-users/zsh-history-substring-search
 
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+_history_substring_search_config() {
+    bindkey '^[[A' history-substring-search-up
+    bindkey '^[[B' history-substring-search-down
 
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+    bindkey -M vicmd 'k' history-substring-search-up
+    bindkey -M vicmd 'j' history-substring-search-down
+}
 
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=green,standout'
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,standout'
