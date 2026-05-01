e00889e60a4db71dac7ea883bdf7ea0a004ce74c
Clinton Montague <clinton3141@gmail.com>
2026-05-01 18:04:44 +0100

    allow custom compdefs

--- config/zsh/syntax-highlighting.zsh
+++ config/zsh/syntax-highlighting.zsh
@@ -9,9 +9,8 @@ zinit ice wait"0" lucid ver"$DOTS_FAST_SYNTAX_HIGHLIGHTING_HASH" atload'
     # Zoxide completion
     command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init zsh --cmd cd)"
 
-    # Mayden completion
-    compdef _mayden_completion mayden 2>/dev/null
-    compdef _mayden_completion m 2>/dev/null
+    # Source any custom compdef registrations
+    for f in ~/.config/dots/custom/*.compdef.zsh(N); do source "$f"; done
 
     # ls/eza completion with zoxide frecent dirs
     command -v zoxide >/dev/null 2>&1 && command -v eza >/dev/null 2>&1 && compdef _ls_zoxide ls eza
