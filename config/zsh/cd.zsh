84b7388745af78051fe10b866f275e22b7fd6d5f
Clinton <clinton3141@gmail.com>
2026-05-01 18:06:54 +0100

    fix zoxide init

--- config/zsh/cd.zsh
+++ config/zsh/cd.zsh
@@ -1,6 +1,4 @@
-# Zoxide - completion will be set up after zicompinit in syntax-highlighting atload
-# Just make zoxide available, don't set up completion yet
-command -v zoxide >/dev/null 2>&1
+command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init zsh --cmd cd)"
 
 # Mix local dirs and zoxide frecent dirs into cd tab completion (compdef registered in syntax-highlighting.zsh after compinit)
 _cd_zoxide() {
